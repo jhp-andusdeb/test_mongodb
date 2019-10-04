cebe82e6ada6f4194b1325dde286e5fd2f053f05
jhp <jhp.andusdeb@gmail.com>
2019-10-04 09:06:27 +0900

    mongodb replset test complete

--- client.go
+++ client.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -45,10 +47,40 @@ func Connect() {
 		fmt.Println("Successfully Connected to MongoDB!")
 	}
 
-	//test := client.Database("anduschain").Collection("test")
+	//sr := client.Database("admin").RunCommand(context.Background(), bson.M{ "replSetGetStatus": 1 } )
+	//data, err := sr.DecodeBytes()
+	//fmt.Println("result : ", data.String())
+for i := 1; i < 1; i++ {
+	sr := client.Database("admin").RunCommand(context.Background(), bson.M{ "isMaster": 1 } )
+	//sr := client.Database("admin").RunCommand(context.Background(), bson.M{ "replSetGetStatus": 1 } )
+	//data, err = sr.DecodeBytes()
+	//fmt.Println("result : ", data.String())
+	//
+	var isMaster bson.Raw
+	sr.Decode(&isMaster)
+	//fmt.Println("isMaster : ", isMaster)
 
+	// 현재의 primary를 lookup
+	fmt.Println("primary is ", isMaster.Lookup("primary").String())
+	fmt.Println("is secondary ", isMaster.Lookup("secondary").String())
+	fmt.Println("I am ", isMaster.Lookup("me").String())
+	fmt.Println("================================")
+
+	time.Sleep(1 * time.Second)
+	//test := client.Database("anduschain").Collection("test")
+}
+
+	var name string
+	tcoll := client.Database("test").Collection("tcoll")
+	//for i := 100000; i < math.MaxInt32; i++ {
+	for i := 0; i < 200000; i++ {
+		if i % 10000 == 0 { fmt.Println(i) }
+		name = fmt.Sprintf("jhp%d", i)
+		tcoll.InsertOne(context.Background(), bson.M{"name":name, "age":i})
+	}
 	if err := client.Disconnect(context.Background()); err != nil {
 		log.Fatal(err)
+		os.Exit(1)
 	}
 	fmt.Println("successfully disconnected")
 }
